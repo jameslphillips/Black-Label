13062ea0ae1d9682d838610ae756dcd44c488869
Bane <19617364+jameslphillips@users.noreply.github.com>
2018-02-13 06:57:21 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -9,12 +9,16 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
+var validator = require('express-validator');
 
 var routes = require('./routes/index');
+var userRoutes = require('./routes/user');
 
 var app = express();
 
 mongoose.connect('localhost:27017/shopping');
+require('./config/passport');
+
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
@@ -24,13 +28,20 @@ app.set('view engine', '.hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
-app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}))
+app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+    res.locals.login = req.isAuthenticated();
+    next();
+});
+
+app.use('/user', userRoutes);
 app.use('/', routes);
 
 // catch 404 and forward to error handler
